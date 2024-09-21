a52466455eca65ec4e6f326b10cdd678bea0176b
Sahariar Islam <sahariar.islam@sjinnovation.com>
2024-09-21 13:11:41 +0600

    useEffecet hook

---
+++ useEffect/useEffect/src/WindowWidth.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function WindowWidth() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth)
+  }
+
+  useEffect(()=>{
+    window.addEventListener('resize',handleResize)
+
+    return()=>{
+      window.removeEventListener('resize',handleResize)
+    }
+  },[])
+
+  return (
+      <div>{windowWidth}</div>
+  )
+}
+
+export default WindowWidth
